162c0604f8579b14063e4c37bdaaa5fb77ac5cf1
gopika-jastack <gopikashree2000@gmail.com>
2026-06-23 19:45:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,52 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "demo-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  address_space = [
+    "10.0.0.0/16"
+  ]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "web-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefixes = [
+    "10.0.1.0/24"
+  ]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "web-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "gopikastorage12345"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
