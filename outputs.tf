29a58084b2845dbbe439435332758ab18ae1baa4
gopika-jastack <gopikashree2000@gmail.com>
2026-06-23 19:46:31 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,11 @@
+output "resource_group" {
+  value = azurerm_resource_group.rg.name
+}
 
+output "vnet_name" {
+  value = azurerm_virtual_network.vnet.name
+}
+
+output "storage_account" {
+  value = azurerm_storage_account.storage.name
+}
